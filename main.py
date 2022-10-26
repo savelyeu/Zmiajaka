866dcc3b697ee4c205485aeca9b02ceee51ca528
Artsiom Savelyeu <42652308+savelyeu@users.noreply.github.com>
2022-10-26 11:24:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,20 +27,21 @@ while game_is_on:
     time.sleep(0.2)
     snake.move()
 
-    if snake.head.distance(food) <15:
+    if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         score.add_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
-
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
 
 
 screen.exitonclick()
